30c7ec7dad51b340d225f31e417c8a561e6ed346
marcosrc92 <47392581+marcosrc92@users.noreply.github.com>
2022-04-08 12:13:01 +0200

    Prototipo de codigo
    No se puede crear el objeto generico de placa porque entra en modo bootloader

--- ControlXYZ_codigoADA/ControlXYZ_codigoADA.ino
+++ ControlXYZ_codigoADA/ControlXYZ_codigoADA.ino
@@ -3,81 +3,56 @@
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 #define Serial SERIAL_PORT_USBVIRTUAL
+#define PASOS_REV 1024
 
-  //creación de objetos Wing
+  //creación del objeto Wing
+  Adafruit_MotorShield f_Axis;
   Adafruit_MotorShield AxisX = Adafruit_MotorShield(0x60);
   Adafruit_MotorShield AxisY = Adafruit_MotorShield(0x61); 
   Adafruit_MotorShield AxisZ = Adafruit_MotorShield(0x62);
+ 
+  //creación del objeto stepper
+   Adafruit_StepperMotor *f_Stepper;
+ 
+const int BUFFER_SIZE = 50;
+byte buf[BUFFER_SIZE];
+unsigned int N_steps;
 
-  //creación de objetos stepper
-  unsigned int N_steps = 1024;
-  Adafruit_StepperMotor *Stepper_X1 = AxisX.getStepper(N_steps, 1);
-  Adafruit_StepperMotor *Stepper_X2 = AxisX.getStepper(N_steps, 2);
-  Adafruit_StepperMotor *Stepper_Y1 = AxisY.getStepper(N_steps, 1);
-  Adafruit_StepperMotor *Stepper_Y2 = AxisY.getStepper(N_steps, 2);
-  Adafruit_StepperMotor *Stepper_Z1 = AxisZ.getStepper(N_steps, 1);
-  Adafruit_StepperMotor *Stepper_Z2 = AxisZ.getStepper(N_steps, 2);
 
 void setup() {
   
   Serial.begin(9600);
+
   AxisX.begin();
   AxisY.begin();
   AxisZ.begin();
 
-  Stepper_X1->release();
-  Stepper_X2->release();
-  Stepper_Y1->release();
-  Stepper_Y2->release();
-  Stepper_Z1->release();
-  Stepper_Z2->release();
+   buf[0] = 0x60; buf[1] = 0x01; buf[2] = 0x01; buf[3] = 0x04; buf[4] = 0x04; buf[5] = 0x00;
 
-
+  f_Stepper = AxisX.getStepper(PASOS_REV, buf[1]);
 }
 
-const int BUFFER_SIZE = 50;
-char buf[BUFFER_SIZE];
 
 void loop() {
-
+/*
   if (Serial.available() > 0) {
-    int rlen = Serial.readBytes(buf, BUFFER_SIZE);
+    int rlen = Serial.readBytes((char*)buf, BUFFER_SIZE);
 
+    
     Serial.print("Se ha recibido: ");
     for(int i = 0; i < rlen; i++)
-      Serial.print(buf[i]);                        
-  }
+      Serial.print(buf[i]); 
+    
 
-  //if(buf[0] == 's'){
-  if(buf[0] == 0x73){
-    Serial.println("Single coil steps");
-    Stepper_X1->step(1024, FORWARD, SINGLE);
-    Stepper_X1->step(1024, BACKWARD, SINGLE);
+    f_Axis = Adafruit_MotorShield(buf[0]);
+    f_Stepper = f_Axis.getStepper(N_steps, buf[1]);
+                             
   }
+*/
 
-  //else if(buf[0] == 'd'){
-  if(buf[0] == 0x64){
-    Serial.println("Double coil steps");
-    Stepper_X1->step(1024, FORWARD, DOUBLE);
-    Stepper_X1->step(1024, BACKWARD, DOUBLE);
-  }
-
-  //else if(buf[0] == 'i'){
-  if(buf[0] == 0x69){
-    Serial.println("Interleave steps");
-    Stepper_X1->step(1024, FORWARD, INTERLEAVE);
-    Stepper_X1->step(1024, BACKWARD, INTERLEAVE);
-  }
-
-  //else if(buf[0] == 'm'){  
-  if(buf[0] == 0x6D){
-    Serial.println("Microstep steps");
-    Stepper_X1->step(1024, FORWARD, MICROSTEP);
-    Stepper_X1->step(1024, BACKWARD, MICROSTEP);
-  }
-
-  else
-    Stepper_X1->release();
+  N_steps = buf[4]*256 + buf[5];
+  f_Stepper->step(N_steps, buf[2], buf[3]);
+  f_Stepper->release();
   
   delay(100);
 }
